c4a85bdf3dbb44054e8085c4d7f9b0b7d5d84470
Jay150703 <jayanthijemi15@gmail.com>
2026-01-29 16:34:17 +0530

    chore : add base OOPSBannerApp class

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPS Banner App
+ *
+ * A console-based Java application to demonstrate
+ * Object-Oriented Programming concepts step by step.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        // UC implementations will be added step by step
+    }
+}
